fa69d471f6219de7221a75935a53a2a2267a7e2f
Ayush kumar <ayushkumar@Ayushs-MacBook-Air.local>
2026-06-16 15:01:20 +0530

    adding data of members

---
+++ js/data.js
@@ -0,0 +1,78 @@
+//data.js
+
+const memberData = {
+
+    member1:{
+        name:"Tanuj Rana",
+        about:"Software Developer",
+        college:"PW IOI",
+        degree:"B.Tech CSE",
+        email:"tanuj.rana.sot25@pwioi.com",
+        github:"https://github.com/Tanuj-Rana",
+        linkedin:"https://www.linkedin.com/in/tanuj-rana-a8645835b/",
+        image:"images/member1.jpg",
+        skills:["HTML","CSS","JavaScript","SQL","Java"]
+    },
+
+    member2:{
+        name:"Rudranil Majumder",
+        about:"Software Developer",
+        college:"PW IOI",
+        degree:"B.Tech CSE",
+        email:"rudranil.majumder.sot25@pwioi.com",
+        github:"https://github.com/rudranilmajumder2006-jpg",
+        linkedin:"https://www.linkedin.com/in/rudranil-majumder-a10120381/",
+        image:"images/member2.jpg",
+        skills:["HTML","CSS","JavaScript","SQL","Java"]
+    },
+
+    member3:{
+        name:"Ayush Kumar",
+        about:"Software Developer",
+        college:"PW IOI",
+        degree:"B.Tech CSE",
+        email:"ayush.kumar.sot25@pwioi.com",
+        github:"https://github.com/ayushkumarsot25-eng",
+        linkedin:"https://www.linkedin.com/in/ayush-kumar-269154304/",
+        image:"images/member3.jpg",
+        skills:["HTML","CSS","JavaScript","SQL","Java"]
+    },
+
+    member4:{
+        name:"Amrita Singh",
+        about:"Software Developer",
+        college:"PW IOI",
+        degree:"B.Tech CSE",
+        email:"amrita.singh.sot25@pwioi.com",
+        github:"https://github.com/amritaxsingh24",
+        linkedin:"https://www.linkedin.com/in/amrita-singh-65722a381/",
+        image:"images/member4.jpg",
+        skills:["HTML","CSS","JavaScript","SQL","Java"]
+    },
+
+    member5:{
+        name:"Abhishek Thakur",
+        about:"Software Developer",
+        college:"PW IOI",
+        degree:"B.Tech CSE",
+        email:"abhishek.thakur.sot25@pwioi.com",
+        github:"https://github.com/Abhi-1606",
+        linkedin:"https://www.linkedin.com/in/abhishek-thakur-46306b381/",
+        image:"images/member5.jpg",
+        skills:["HTML","CSS","JavaScript","SQL","Java"]
+    },
+
+    member6:{
+        name:"Gaurav Kalal",
+        about:"Software Developer",
+        college:"PW IOI",
+        degree:"B.Tech CSE",
+        email:"member6@gmail.com",
+        github:"https://github.com",
+        linkedin:"https://www.linkedin.com/in/gaurav-kalal-58b7b5384/",
+        image:"images/member6.jpg",
+        skills:["HTML","CSS","JavaScript","SQL","Java"]
+    }
+
+};
+
